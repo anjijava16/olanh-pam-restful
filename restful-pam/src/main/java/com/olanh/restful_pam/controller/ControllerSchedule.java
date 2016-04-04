9c13e712ec4dadc8c9cb4234c3733d486d45bb2a
olanhect <olanhector@gmail.com>
2016-04-04 08:03:08 -0400

    New implementations

--- restful-pam/src/main/java/com/olanh/restful_pam/controller/ControllerSchedule.java
+++ restful-pam/src/main/java/com/olanh/restful_pam/controller/ControllerSchedule.java
@@ -4,6 +4,7 @@ import java.net.URISyntaxException;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,9 +15,9 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.Status;
 
-import com.olanh.olanh_entities.Place;
 import com.olanh.olanh_entities.Schedule;
 import com.olanh.olanh_entities.status.StatusGet;
+import com.olanh.olanh_entities.status.StatusUpdate;
 import com.olanh.pam_dataaccess.hibernate.DAOSchedule;
 import com.olanh.pam_dataaccess.util.DAOResponseUtil;
 
@@ -34,30 +35,49 @@ public class ControllerSchedule {
 		if (daoResponse.getStatus() == StatusGet.OK){
 			Schedule schedule = daoResponse.getResponse();
 			schedule.addLink(this.getUriForSelf(uriInfo, schedule),"self");
-			schedule.addLink(this.getUriForSelfPlace(uriInfo, schedule),"via");
+			schedule.addLink(this.getUriForSelfPlace(uriInfo, placeId),"via");
 			return Response.ok(schedule).build();
 		} else if (daoResponse.getStatus() == StatusGet.NOT_FOUND){
 			return Response.status(Status.NOT_FOUND).build();
 		}
 		// TEMP
-		daoResponse.getResponse().addLink("www.com" + placeId,"self");
+		daoResponse.getResponse().addLink(this.getUriForSelf(uriInfo, daoResponse.getResponse()),"selfOffline");
+		daoResponse.getResponse().addLink(this.getUriForSelfPlace(uriInfo, placeId),"via");
 		return Response.serverError().entity(daoResponse.getResponse()).build();
 		//return Response.serverError().build();
 	}
+
+	// UPDATE
+	@PUT
+	public Response updatePlace(@PathParam("placeId") long placeId, Schedule schedule) {
+		DAOResponseUtil<StatusUpdate, Schedule> daoResponse = ControllerSchedule.daoSchedule.updateSchedule(schedule);
+		if (daoResponse.getStatus() == StatusUpdate.UPDATED){
+			Schedule scheduleUpdated= daoResponse.getResponse();
+			scheduleUpdated.setId(placeId);
+			return Response.status(Status.ACCEPTED).entity(scheduleUpdated).build();
+		}else if (daoResponse.getStatus() == StatusUpdate.NOT_FOUND){
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		// TEMP
+		schedule.setId(placeId);
+		return Response.serverError().entity(schedule).build();
+		//return Response.serverError().build();
+	}
+
 	
 
 	// Private Methods
 	private String getUriForSelf(UriInfo uriInfo, Schedule schedule) {
 		String url = uriInfo.getAbsolutePathBuilder()
-							//.path(ControllerPlace.class)
-							.path(Long.toString(schedule.getId()))
 							.build().toString();
 		return url;
 	}
-	private String getUriForSelfPlace(UriInfo uriInfo, Schedule schedule) {
-		String url = uriInfo.getAbsolutePathBuilder()
-							//.path(ControllerPlace.class)
-							.build().toString();
+
+	private String getUriForSelfPlace(UriInfo uriInfo, long placeId) {
+		String url = uriInfo.getBaseUriBuilder()
+				.path(ControllerPlace.class)
+				.path(placeId+"")
+				.build().toString();
 		return url;
 	}
 	
